ca63d1672a8189113fd764a04718d8084cfa834e
gToye <43028004+gToye@users.noreply.github.com>
2018-10-01 09:37:54 +0200

    transformaties

---
+++ variabelen/Transformaties.py
@@ -0,0 +1,5 @@
+a = float(input('geef waarde van a:'))
+b = float(input('geef waarde van b:'))
+print('f(x) = 2(x - 3)^2 + 4')
+print('g(x) = 2(x - ' + str(-1*int(-3-a)) + ')^2 + ' +  str(int(4+b)))
+
--- variabelen/avogrado.py
+++ variabelen/avogrado.py
@@ -1,7 +1,8 @@
-n = float(input(' Geef aantal deeltjes s: '))
+n = float(input( ' Geef aantal deeltjes s:' ))
 na = 6.020 * (10 ** 23)
-m(s) = 32.06
-m(x) =  m(s)/n
-n(x) = n * na
-print(n(x))
-print(m(x))
+ms = 32.06
+mx =  ms * n
+nx = n * na
+print(mx)
+print(nx)
+
